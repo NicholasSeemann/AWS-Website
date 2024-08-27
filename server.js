93957ed12cbf46bfa3d99eeebe553c4b2b5d370f
Nicholas Seemann <nseemann4740@gmail.com>
2024-08-27 16:29:40 -0400

    Add variable to allow enviornment to use preferred port instead of 3000

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+
 app.get('/', (req, res) => {
     res.send("Welcome to the home page!");
 });
 
-app.listen(3000, () => {
+const port = process.env.port || 3000;
+app.listen(port, () => {
     console.log("Hello friends!");
 });
